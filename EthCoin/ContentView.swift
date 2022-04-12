ab8ee6fd05d929ca5c063479d0c614fbea3b2cd7
andy <a5706617@gmail.com>
2022-04-12 17:34:48 +0800

    Initial Commit

---
+++ EthCoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EthCoin
+//
+//  Created by 江啟綸 on 2022/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EthCoin/EthCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthCoinApp.swift
+//  EthCoin
+//
+//  Created by 江啟綸 on 2022/4/12.
+//
+
+import SwiftUI
+
+@main
+struct EthCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
